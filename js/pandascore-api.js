8b870bfbfde56cda07a99368d2b08ce3272ef065
Sylent <sylentbutdeadly@gmail.com>
2019-10-08 23:18:39 -0400

    Ok make async false

--- js/pandascore-api.js
+++ js/pandascore-api.js
@@ -3,18 +3,17 @@ function getData(reference) {
     var resp = "";
     var xmlHttp = new XMLHttpRequest();
 
-    // if ("withCredentials" in xmlHttp) {
-    //     xmlHttp.open("GET", url, true);
-    // } else if (typeof XDomainRequest != "undefined") {
-    //     xmlHttp = new XDomainRequest();
-    //     xmlHttp.open("GET", url);
-    // } else {
-    //     xmlHttp = null;
-    // }
+    if ("withCredentials" in xmlHttp) {
+        xmlHttp.open("GET", url, false);
+    } else if (typeof XDomainRequest != "undefined") {
+        xmlHttp = new XDomainRequest();
+        xmlHttp.open("GET", url);
+    } else {
+        xmlHttp = null;
+    }
 
     if (xmlHttp != null) {
-        xmlHttp.open("GET", url, true);
-        xmlHttp.send(null);
+        xmlHttp.send();
         return xmlHttp.responseText;
     } else {
         return "Something went wrong!";
