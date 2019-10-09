ed5f038ea9fbc5c567ac4029e1844516d78e18ae
Sylent <sylentbutdeadly@gmail.com>
2019-10-08 23:17:33 -0400

    Try this

--- js/pandascore-api.js
+++ js/pandascore-api.js
@@ -1,16 +1,22 @@
 function getData(reference) {
-    var resp ;
-    var xmlHttp ;
+    var url = "https://api.pandascore.co" + reference + "/?token=JRJdwSk8cheV3hM2yk_jbNEL3NxFv3RBQ0cazrOAs3ceMs67vvU";
+    var resp = "";
+    var xmlHttp = new XMLHttpRequest();
 
-    resp  = '' ;
-    xmlHttp = new XMLHttpRequest();
+    // if ("withCredentials" in xmlHttp) {
+    //     xmlHttp.open("GET", url, true);
+    // } else if (typeof XDomainRequest != "undefined") {
+    //     xmlHttp = new XDomainRequest();
+    //     xmlHttp.open("GET", url);
+    // } else {
+    //     xmlHttp = null;
+    // }
 
-    if(xmlHttp != null)
-    {
-        xmlHttp.open( "GET", "https://api.pandascore.co" + reference + "/?token=JRJdwSk8cheV3hM2yk_jbNEL3NxFv3RBQ0cazrOAs3ceMs67vvU", false );
-        xmlHttp.send( null );
-        resp = xmlHttp.responseText;
+    if (xmlHttp != null) {
+        xmlHttp.open("GET", url, true);
+        xmlHttp.send(null);
+        return xmlHttp.responseText;
+    } else {
+        return "Something went wrong!";
     }
-
-    return resp;
 }
