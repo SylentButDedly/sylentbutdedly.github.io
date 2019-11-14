eb77f6855fb514557ea886daf87fd1a263d78fc2
Sylent <sylentbutdeadly@gmail.com>
2019-11-14 15:50:43 -0500

    Make API call ambiguous

--- js/pandascore-api.js
+++ js/pandascore-api.js
@@ -2,28 +2,30 @@ function getData(reference) {
     const url = "https://api.pandascore.co" + reference + "/?token=JRJdwSk8cheV3hM2yk_jbNEL3NxFv3RBQ0cazrOAs3ceMs67vvU";
 
     var request = new XMLHttpRequest();
-    request.open('GET', url, true);
+    request.open('GET', url, false);
 
-    request.onload = function() {
-        var data = JSON.parse(this.response);
-
-        if (request.status < 200 || request.status > 400) {
-            console.log("Something went wrong! Error Code: " + request.status);
-            return "Something went wrong!";
-        }
-
-        let heroes = "";
-        data.forEach(item => {
-            heroes += "<h2>" + item.name + "</h2>" +
-                "<p>Real Name: " + item.real_name + "</p>" +
-                "<p>Role: " + item.role + "</p>" +
-                "<p>Difficulty: " + item.difficulty + "</p>";
-        });
-        document.getElementById("main").innerHTML = heroes;
-
-        return data;
-    };
-
+    // request.onload = function() {
+    //     var data = JSON.parse(this.response);
+    //
+    //     if (request.status < 200 || request.status > 400) {
+    //         console.log("Something went wrong! Error Code: " + request.status);
+    //         return "Something went wrong!";
+    //     }
+    //
+    //     console.log("Done with request");
+    //     return data;
+    // };
+    //
     request.send();
 
+    console.log("Response Text: " + request.responseText);
+    let data = JSON.parse(request.responseText);
+
+    if (request.status < 200 || request.status > 400) {
+        console.log("Something went wrong! Error Code: " + request.status);
+        return "Something went wrong!";
+    }
+
+    console.log("Done with request");
+    return data;
 }
